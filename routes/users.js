d4442c4c851496655fa6f29faf8c420ab24042bf
Paulo Astorga <pauloastorga.docs@gmail.com>
2023-08-23 22:55:20 -0400

    Initial Commit

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send("AAAAAAA");
+});
+
+module.exports = router;
